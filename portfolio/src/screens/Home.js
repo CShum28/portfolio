4f2aa1d6a531485fcbc05e61a0df93dfa56b500d
Clement <clement.shum128@gmail.com>
2023-11-29 20:07:16 -0500

    home page

---
+++ portfolio/src/screens/Home.js
@@ -0,0 +1,46 @@
+import React, { useRef } from "react";
+import Header from "../components/Header";
+import FrontPage from "../components/FrontPage";
+import About from "../components/About";
+import Portfolio from "../components/Portfolio";
+import Contact from "../components/Contact";
+import Skills from "../components/Skills";
+import SideBar from "../components/SideBar";
+
+const Home = () => {
+  const frontPageRef = useRef(null);
+  const aboutRef = useRef(null);
+  const portfolioRef = useRef(null);
+  const contactRef = useRef(null);
+
+  return (
+    <div>
+      <Header
+        frontPageRef={frontPageRef}
+        aboutRef={aboutRef}
+        portfolioRef={portfolioRef}
+        contactRef={contactRef}
+      />
+      <SideBar />
+      <div className="w-6/12 mx-auto">
+        <div ref={frontPageRef} className="h-screen w-full">
+          <FrontPage portfolioRef={portfolioRef} />
+        </div>
+        <div ref={aboutRef}>
+          <About className="w-full h-screen " />
+        </div>
+        <div>
+          <Skills className="w-full h-screen " />
+        </div>
+        <div ref={portfolioRef}>
+          <Portfolio className="w-full h-screen" />
+        </div>
+        <div ref={contactRef}>
+          <Contact className="w-full h-screen " />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
